4691ac24ac1f45f948584a4d9d6ea538186eb552
Kaustav Banerjee <kaustavbanerjee96@gmail.com>
2026-01-06 20:48:21 +0530

    Updating email for test

--- src/email_gmail.py
+++ src/email_gmail.py
@@ -5,8 +5,8 @@ from email.mime.text import MIMEText
 def send_email(subject, html):
     sender = os.getenv("GMAIL_USER")
     password = os.getenv("GMAIL_APP_PASSWORD")
-    receiver = "himasreedam@gmail.com"
-    # receiver = "1415016@kiit.ac.in"
+    # receiver = "himasreedam@gmail.com"
+    receiver = "1415016@kiit.ac.in"
 
     msg = MIMEText(html, "html")
     msg["Subject"] = subject
